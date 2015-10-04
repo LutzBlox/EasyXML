f9377316e90a8dfc262abf30ec75f8c015e06f14
Christopher Lutz <lutzblox@gmail.com>
2015-10-04 16:12:32 -0400

    Removed multi-catch statements

--- src/main/java/com/github/lutzblox/easyxml/readers/XMLReader.java
+++ src/main/java/com/github/lutzblox/easyxml/readers/XMLReader.java
@@ -12,73 +12,92 @@ import org.xml.sax.SAXException;
 
 import com.github.lutzblox.easyxml.trees.XMLTree;
 
+
 public abstract class XMLReader {
-
+	
 	private ReaderParameters params;
-
+	
 	private DocumentBuilderFactory factory;
-
+	
 	public XMLReader() {
 		
 		this(new ReaderParameters());
 	}
-
+	
 	public XMLReader(ReaderParameters params) {
-
+		
 		this.params = params;
-
+		
 		factory = DocumentBuilderFactory.newInstance();
 		factory.setIgnoringComments(params.getIgnoreComments());
 		factory.setIgnoringElementContentWhitespace(params.getIgnoreWhitespace());
 		factory.setValidating(params.getValidate());
 		factory.setNamespaceAware(params.getNamespaceAware());
 	}
-
+	
 	public XMLTree read(File file) throws IOException {
-
+		
 		try {
-
+			
 			return read(factory.newDocumentBuilder().parse(file));
-
-		} catch (SAXException | ParserConfigurationException e) {
-
+			
+		} catch (SAXException e) {
+			
 			e.printStackTrace();
-
+			
+			return new XMLTree();
+			
+		} catch (ParserConfigurationException e) {
+			
+			e.printStackTrace();
+			
 			return new XMLTree();
 		}
 	}
-
+	
 	public XMLTree read(InputStream stream) throws IOException {
-
+		
 		try {
-
+			
 			return read(factory.newDocumentBuilder().parse(stream));
-
-		} catch (SAXException | ParserConfigurationException e) {
-
+			
+		} catch (SAXException e) {
+			
 			e.printStackTrace();
-
+			
+			return new XMLTree();
+			
+		} catch (ParserConfigurationException e) {
+			
+			e.printStackTrace();
+			
 			return new XMLTree();
 		}
 	}
-
+	
 	public XMLTree read(String str) throws IOException {
-
+		
 		try {
-
+			
 			return read(factory.newDocumentBuilder().parse(str));
-
-		} catch (SAXException | ParserConfigurationException e) {
-
+			
+		} catch (SAXException e) {
+			
 			e.printStackTrace();
-
+			
+			return new XMLTree();
+			
+		} catch (ParserConfigurationException e) {
+			
+			e.printStackTrace();
+			
 			return new XMLTree();
 		}
 	}
-
+	
 	public abstract XMLTree read(Document d);
 	
-	public ReaderParameters getReaderParameters(){
+	public ReaderParameters getReaderParameters() {
 		
 		return params;
 	}
